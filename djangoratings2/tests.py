ec46df0d5fa76b91c6401492e607a057d88a90b4
Patrick Taylor <patricktaylor@mac.com>
2013-01-30 23:47:55 -0400

    Add custom User model support to tests.py

--- djangoratings2/tests.py
+++ djangoratings2/tests.py
@@ -2,13 +2,21 @@ import unittest
 import random
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 
 from exceptions import *
 from models import Vote, SimilarUser, IgnoredObject
 from fields import AnonymousRatingField, RatingField
+
+# support for custom User models in Django 1.5+
+try:
+    from django.contrib.auth import get_user_model
+except ImportError:  # django < 1.5
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
 
 settings.RATINGS_VOTES_PER_IP = 1
 
